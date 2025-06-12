4c26b39eb37b05e251df1338af87b4b745368276
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-06-12 15:15:10 +0100

    Added function for fetching a single invoice by id

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-type Invoice = {
+export type Invoice = {
   id: string;
   // _id?: string;
   _id: string;
--- frontend/src/InvoiceDetail.tsx
+++ frontend/src/InvoiceDetail.tsx
@@ -1,12 +1,33 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import type { Invoice } from "./Dashboard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const InvoiceDetail = () => {
+  const [invoice, setInvoice] = useState<Invoice | null>(null);
+  const { id } = useParams();
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`http://localhost:3000/invoice/${id}`)
+        .then((response) => setInvoice(response.data))
+        .catch((error) => console.error(error));
+    }
+  }, [id]);
+  invoice? console.log(invoice.clientEmail) : []
 
-    const { id } = useParams()
-    console.log("Invoice ID from URL:", id);
   return (
-    <div>InvoiceDetail for ID: {id}</div>
-  )
-}
+    <div className="invoice-detail-container">
+      <div className="goBack-arrow-div">
+        <img src="/images/arrow-left.svg" alt="arrow left" />
+        <p>Go back</p>
+      </div>
+      <div className="invoiceDetail-header">
+       
+        </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default InvoiceDetail
+export default InvoiceDetail;
