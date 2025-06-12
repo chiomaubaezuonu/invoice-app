f013cb9613923228544523683b68c557eb82cc4f
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-06-12 10:57:56 +0100

    Implemented responsive sidebar with layered logo styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="container">
+        <div className="sidebar">
+          <div className="logo-div">
+            <img src="/images/logo.svg" className="logo" alt="" />
+          </div>
+          <div className="sidebar-content-div">
+            <div className="moon-div">
+              <img
+                src="/images/icon-moon.svg"
+                className="moon-img"
+                alt="moon"
+              />
+            </div>
+            <div className="user-div">
+              <img src="/images/user.jpg" alt="user" className="user-img" />
+            </div>
+          </div>
+        </div>
+        <div className="main">
+          <div className="main-navbar">
+            <div>
+              <h1>Invoices</h1>
+              <p>There are 8 total invoices </p>
+            </div>
+            <div></div>
+            <div></div>
+            <button>New Invoice</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
