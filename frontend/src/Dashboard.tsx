86ba729629e8e9eee4b8b00b9a4ce00a5bd8e979
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-06-23 15:15:11 +0100

    Added layout for new invoice and function for  submitting form

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,7 +1,8 @@
-import { Divider } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import type { DatePickerProps } from "antd";
+import { DatePicker, Space } from "antd";
 
 export type Invoice = {
   id: string;
@@ -36,6 +37,15 @@ export type Invoice = {
 function Dashboard() {
   const [invoices, setInvoices] = useState<Invoice[]>([]);
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const [formData, setFormdata] = useState({
+    clientName: "",
+    paymentDue: "",
+    streetAddress: "",
+    projectDescription: "",
+    description: "",
+    paymentTerms: 0,
+    clientEmail: "",
+  });
 
   useEffect(() => {
     axios
@@ -43,6 +53,31 @@ function Dashboard() {
       .then((response) => setInvoices(response.data))
       .catch((error) => console.error(error));
   }, []);
+
+  const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/invoice", formData);
+
+    // setFormdata({
+    //   clientName: "",
+    //   paymentDue: "",
+    //   streetAddress: "",
+    //   projectDescription: "",
+    //   description: "",
+    //   paymentTerms: 0,
+    //   clientEmail: "",
+    // });
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormdata((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   return (
     <div>
@@ -122,13 +157,199 @@ function Dashboard() {
           )}
           {isFormOpen && (
             <div className={`invoice-form-div${isFormOpen ? " open" : ""}`}>
-              <form action="">
-              <h2>Create Form</h2>
-              <h4 className="form-subText">Bill From</h4>
-              <div className="form-input-grp-col">
-                <label className="form-label-grp" htmlFor="streetAddress">Street Address</label>
-                <input type="text" placeholder="street address" required />
-              </div>
+              <form onSubmit={handleForm}>
+                <h2>Create Form</h2>
+                <h4 className="form-subText">Bill From</h4>
+                <div className="form-input-grp-col">
+                  <label className="form-label-grp" htmlFor="streetAddress">
+                    Street Address
+                  </label>
+                  <input
+                    name="streetAddress"
+                    type="text"
+                    className="input"
+                    placeholder="street address"
+                    required
+                  />
+                </div>
+                <div className="form-input-grp-row">
+                  <div className="form-row-div">
+                    <label htmlFor="city" className="form-label-grp">
+                      City
+                    </label>
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="city"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-row-div">
+                    <label htmlFor="post-code" className="form-label-grp">
+                      Post Code
+                    </label>
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="post-code"
+                      required
+                    />
+                  </div>
+                  <div className="form-row-div">
+                    <label htmlFor="country" className="form-label-grp">
+                      Country
+                    </label>
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="country"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="form-client-info">
+                  <h4 className="form-subText">Bill To</h4>
+                  <div className="form-input-grp-col">
+                    <label className="form-label-grp" htmlFor="streetAddress">
+                      Client's Name
+                    </label>
+                    <input
+                      name="clientName"
+                      type="text"
+                      className="input"
+                      placeholder="client's name"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-input-grp-col">
+                    <label className="form-label-grp" htmlFor="clientEmail">
+                      Client's Email
+                    </label>
+                    <input
+                      name="clientEmail"
+                      type="text"
+                      className="input"
+                      placeholder="street address"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-input-grp-col">
+                    <label className="form-label-grp" htmlFor="clientEmail">
+                      Street Address
+                    </label>
+                    <input
+                      name="streetAddress"
+                      type="text"
+                      className="input"
+                      placeholder="street address"
+                      onChange={handleInputChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-input-grp-row">
+                    <div className="form-row-div">
+                      <label htmlFor="city" className="form-label-grp">
+                        City
+                      </label>
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="city"
+                        onChange={handleInputChange}
+                        required
+                      />
+                    </div>
+                    <div className="form-row-div">
+                      <label htmlFor="post-code" className="form-label-grp">
+                        Post Code
+                      </label>
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="post-code"
+                        required
+                      />
+                    </div>
+                    <div className="form-row-div">
+                      <label htmlFor="country" className="form-label-grp">
+                        Country
+                      </label>
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="country"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="form-payment-row">
+                  <div className="form-row-div">
+                    <label htmlFor="invoiceDate">Invoice Date</label>
+                    <Space direction="vertical">
+                      <DatePicker onChange={onChange} />
+                    </Space>
+                  </div>
+                  <div className="form-row-div">
+                    <label htmlFor="invoiceDate">Payment Terms</label>
+                    <select name="" id="" className="select">
+                      Next 30 Days
+                      <option value=""> Next 30 Days </option>
+                      <option value=""> Next 14 Days </option>
+                      <option value=""> Next 7 Days </option>
+                      <option value=""> Next 1 Day </option>
+                    </select>
+                  </div>
+                </div>
+                <div className="form-input-grp-col">
+                  <label className="form-label-grp" htmlFor="project-desc">
+                    Project Description
+                  </label>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="project description"
+                    required
+                  />
+                </div>
+                <div>
+                  <h2>Item List</h2>
+                  <div>
+                    <p>Item Name</p>
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="project description"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <p>Qty</p>
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="project description"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <p>Price</p>
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="project description"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <p>Total</p>
+                    <img src="/images/icon-delete.svg" alt="icon-delete" />
+                  </div>
+                </div>
+                <button className="add-new-invoice-btn">+ Add New Item</button>
               </form>
             </div>
           )}
